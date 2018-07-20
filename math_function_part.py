aff662dcab94f7b9c6f1a67ee9bf6b0c8107f56e
ccpjsg <ccpjsg@gmail.com>
2018-07-20 15:55:14 +0800

    add LCM 最小公倍数

--- math_function_part.py
+++ math_function_part.py
@@ -5,6 +5,7 @@ Created on Fri Jul 20 13:09:29 2018
 @author: Administrator
 """
 
+#最大公约数，辗转相除法
 def gcd(a, b):
     if a < b:
         a, b = b, a
@@ -14,3 +15,15 @@ def gcd(a, b):
         a = b
         b = temp
     return a
+
+#lcm = a*b/gcd,即两数乘积/最大公约数
+def lcm(a, b):
+    t = a*b
+    if a < b:
+        a, b = b, a
+    
+    while b != 0:
+        temp = a % b
+        a = b
+        b = temp
+    return t/a
